48e9c3f56f7496881cdfe346283c9849233e8558
Roman Soproniuk <anteumbra@gmail.com>
2024-05-27 17:45:37 +0300

    create base tg bot

---
+++ src/main/java/com/rsoproniuk/currencytelegrambot/CurrencyTelegramBot.java
@@ -0,0 +1,35 @@
+package com.rsoproniuk.currencytelegrambot;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
+import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+@Component
+@RequiredArgsConstructor
+public class CurrencyTelegramBot implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
+    private final TelegramClient telegramClient;
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public LongPollingUpdateConsumer getUpdatesConsumer() {
+        return this;
+    }
+
+    @SneakyThrows
+    @Override
+    public void consume(Update update) {
+
+    }
+}
